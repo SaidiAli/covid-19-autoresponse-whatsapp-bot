8d69a085a6b7599e930a9dd8fdcc3cd83c8aa442
Isiko saidiali <isikosaidiali@gmail.com>
2020-04-12 19:38:49 +0300

    init

--- app/Http/Controllers/OutBoundMessageController.php
+++ app/Http/Controllers/OutBoundMessageController.php
@@ -8,9 +8,13 @@ use Twilio\Rest\Client;
 
 class OutBoundMessageController extends Controller
 {
+    protected $sandbox_numbers = [
+        '+256777343212', '+256750783581'
+    ];
+
     public function index() {
-        $sid = "AC692ec0f20baeaaa540be376063dc8149";
-        $token = "6961004c5a711cc1683c69664c6b91ae";
+        $sid = env('TWILIO_SID');
+        $token = env('TWILIO_TOKEN');
         $twilio = new Client($sid, $token );
 
         $response = Http::get('https://api.covid19api.com/summary')->json();
@@ -18,16 +22,18 @@ class OutBoundMessageController extends Controller
         foreach ($response['Global'] as $key => $value) {
             $msg[] = $key.' : '. $value . "\n";
         }
-        $message = "Here is a summary of the situation: " . implode("\n", $msg);
+        $message = "Here is the summary for the covid-19 situation as of today: \n" . implode("\n", $msg);
 
-        $message = $twilio->messages->create(
-            'whatsapp:+256777343212',
-            [
-                'from' => 'whatsapp:+14155238886',
-                'body' => $message
-            ]
+        foreach($this->sandbox_numbers as $value) {
+            $message = $twilio->messages->create(
+                'whatsapp:'.$value,
+                [
+                    'from' => 'whatsapp:+14155238886',
+                    'body' => $message
+                ]
             );
 
-            return $message->sid;
+            $message->sid;
+        }
     }
 }
