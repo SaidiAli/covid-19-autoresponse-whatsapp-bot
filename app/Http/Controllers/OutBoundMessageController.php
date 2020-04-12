b3d2173ccf649ac3903dc148cae65b0201e6d951
Isiko saidiali <isikosaidiali@gmail.com>
2020-04-12 15:06:42 +0300

    added corona update

--- app/Http/Controllers/OutBoundMessageController.php
+++ app/Http/Controllers/OutBoundMessageController.php
@@ -3,20 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Twilio\Rest\Client;
 
 class OutBoundMessageController extends Controller
 {
     public function index() {
-        $sid = env('TWILIO_SID');
-        $token = env('TWILIO_TOKEN');
+        $sid = "AC692ec0f20baeaaa540be376063dc8149";
+        $token = "6961004c5a711cc1683c69664c6b91ae";
         $twilio = new Client($sid, $token );
+
+        $response = Http::get('https://api.covid19api.com/summary')->json();
+
+        foreach ($response['Global'] as $key => $value) {
+            $msg[] = $key.' : '. $value . "\n";
+        }
+        $message = "Here is a summary of the situation: " . implode("\n", $msg);
 
         $message = $twilio->messages->create(
             'whatsapp:+256777343212',
             [
                 'from' => 'whatsapp:+14155238886',
-                'body' => 'Hello from twilio laravel'
+                'body' => $message
             ]
             );
 
--- string_test.php
+++ string_test.php
@@ -1,27 +1,11 @@
 <?php
-$str1 = "hello";
-$str2 = "wakiso Rdc Prevent";
-$arr = explode(' ', $str2);
-$test = [
-    'rdc' => 'http://rdc_list_link.com',
-    'symptoms' => "Theses are some of the symptoms for the covid-19 disease: \n --- \n if you're experiencing any of these, please see a medical personel or call on the ministry of Health toll free lines to get aid: 0800203033",
-    'prevent' => "These are some of the ways you can prevent yourself from the deadly coronavirus:",
-    'helplines' => "These are the Covid-19 official toll free helplines: \n 0800203033"
+$arr = [
+    'one' => 1,
+    'two' => 2,
+    'three' => 3
 ];
 
-foreach ($test as $key => $msg) {
-    foreach($arr as $val) {
-        if (strtolower($val) == $key) {
-            print_r($msg."\n");
-        }else {
-            continue;
-        }
-    }
+$str = implode(' ', $arr);
+foreach ($arr as $key => $value) {
+    print_r($key.' : '. $value."\n");
 }
-
-
-// $new = chunk_split($string, 5);
-// print_r(str_split(trim($string)));
-
-// print_r(count(explode(' ', trim($string))));
-// print_r(implode('-', explode(' ', trim($string))));
