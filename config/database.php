444aff7e8a75b670f512b6a81105b0ee3968521b
Isiko saidiali <isikosaidiali@gmail.com>
2020-04-26 15:31:46 +0300

    init

--- config/database.php
+++ config/database.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Str;
-$url = parse_url(env("CLEARDB_DATABASE_URL"));
 
 return [
 
@@ -46,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env("CLEARDB_DATABASE_URL"),
-            'host' => $url['host'],
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => substr($url['path'], 1),
-            'username' => $url['user'],
-            'password' => $url['pass'],
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
