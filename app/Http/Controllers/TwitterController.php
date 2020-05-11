b01cf131f37b526b7ceda0944e813c769442c28d
Isiko saidiali <isikosaidiali@gmail.com>
2020-05-11 14:14:24 +0300

    init

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -10,7 +10,8 @@ class TwitterController extends Controller
         // Challenge-Response Checks
         $crc_token = $req->query('crc_token');
         $sha256_hash = hash_hmac('sha256', $crc_token, env('TWITTER_SECRET_KEY'));
+        $res = 'sha256=' . base64_encode($sha256_hash);
 
-        return response()->json(['response_token' => 'sha256' + base64_encode($sha256_hash)]);
+        return response()->json(['response_token' => $res]);
     }
 }
